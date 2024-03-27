59bdd3551d0e9c6e1f23a8600733c4c36b3dbfb9
NebulaEclipse <chrisdouglas200516@gmail.com>
2024-03-27 17:18:56 -0400

    Added Profile page and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import Home from './pages/home'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import Navbar from "./components/Navbar";
+import Home from './pages/home';
+import Projects from './pages/Projects';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Home />
-    </>
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
